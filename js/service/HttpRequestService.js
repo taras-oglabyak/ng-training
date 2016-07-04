016b5c2929fc2f344af65f94dbad60199c492839
Taras Oglabyak <taras.oglabyak@gmail.com>
2016-07-04 09:40:05 +0300

    Added logs to httpRequestService

--- js/service/HttpRequestService.js
+++ js/service/HttpRequestService.js
@@ -14,30 +14,35 @@
 
         this.findOne = function(entityName, id) {
             var url = globalConstant.HOST + entityName + '/getRecords/' + id;
+            $log.info('requested: ' + url);
 
             return $http.get(url);
         };
 
         this.count = function(entityName) {
             var url = globalConstant.HOST + entityName + '/countRecords';
+            $log.info('requested: ' + url);
 
             return $http.get(url);
         };
 
         this.delete = function(entityName, id) {
             var url = globalConstant.HOST + entityName + '/del/' + id;
+            $log.info('requested: ' + url);
 
             return $http.get(url);
         };
 
         this.update = function(entityName, entity, id) {
             var url = globalConstant.HOST + entityName + '/update/' + id;
+            $log.info('requested: ' + url);
 
             return $http.post(url, entity);
         };
 
         this.create = function(entityName, entity) {
             var url = globalConstant.HOST + entityName + '/insertData';
+            $log.info('requested: ' + url);
 
             return $http.post(url, entity);
         };
