f5ffd670e78ec69d48334d879ab19895a32806b2
Taras Oglabyak <taras.oglabyak@gmail.com>
2016-07-05 09:52:39 +0300

    Added Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 
-var jshint = require('gulp-jshint');
+var gp_concat = require('gulp-concat');
+var gp_rename = require('gulp-rename');
+var gp_uglify = require('gulp-uglify');
+var gp_cssmin = require('gulp-cssmin');
 
 gulp.task('lint', function() {
     return gulp.src('js/**/*.js')
@@ -8,5 +11,35 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
+gulp.task('js-min', function(){
+    return gulp.src(
+             ['js/**/*.js',
+              'bower_components/angular/angular.js',
+              'bower_components/angular-route/angular-route.js',
+              'bower_components/angular-growl-v2/build/angular-growl.js',
+              'bower_components/angularUtils-pagination/dirPagination.js'])
+        .pipe(gp_concat('main.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('dist'));
+});
 
-gulp.task('default', ['lint']);
+gulp.task('css-concat', function(){
+    return gulp.src(
+             ['bower_components/angular-growl-v2/build/angular-growl.css',
+              'bower_components/bootstrap/dist/css/bootstrap.css',
+              'css/main.css'])
+        .pipe(gp_concat('main.css'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gp_cssmin())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('index-copy', function () {
+     gulp
+      .src('index-prod.html')
+      .pipe(gp_rename('index.html'))
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['js-min', 'css-concat', 'index-copy']);
